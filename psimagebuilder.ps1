ec9a0b16d2bd904ade5a40251bc6c1bce541f624
Sourav Poddar <sp.198713@gmail.com>
2025-05-25 00:21:35 +0530

    Update 006

--- psimagebuilder.ps1
+++ psimagebuilder.ps1
@@ -69,3 +69,15 @@ $RoleAssignParams = @{
     Scope              = "/subscriptions/$subscriptionID/resourceGroups/$imageResourceGroup"
 }
 New-AzRoleAssignment @RoleAssignParams
+
+
+$sigGalleryName = "MyImageGallary"
+$imageDefName = "win11avd"
+
+# Create the gallery
+New-AzGallery -GalleryName $sigGalleryName -ResourceGroupName $imageResourceGroup  -Location $location
+
+# Create the gallery definition
+New-AzGalleryImageDefinition -GalleryName $sigGalleryName -ResourceGroupName $imageResourceGroup -Location $location -Name $imageDefName -OsState generalized -OsType 'Windows' -Publisher 'myCo' -Offer 'Windows' -Sku '10avd'
+
+
