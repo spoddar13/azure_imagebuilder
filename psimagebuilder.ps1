73e4a310dda28a13f2985d0889d8c6e786dbcd7b
Sourav Poddar <sp.198713@gmail.com>
2025-05-24 23:55:23 +0530

    Update 003

--- psimagebuilder.ps1
+++ psimagebuilder.ps1
@@ -1,51 +1,38 @@
 #Connect-AzAccount
 
-#Set Subscription
-$subscriptionID = (Get-AzContext).Subscription.Id
-Set-AzContext -SubscriptionId $subscriptionID
+
 
 #Timestamp
 $timestamp = Get-Date -Format "yyyyMMdd-HHmmss"
 
-# Destination image resource group name
-$imageResourceGroup = 'RG-' + $timestamp
+# Step 1: Import module
+Import-Module Az.Accounts
 
-# Azure region
-$location = 'centralindia'
+# Step 2: get existing context
+$currentAzContext = Get-AzContext
 
-# Name of the image to be created
-$imageTemplateName = 'myWinImage'
+# Destination image resource group
+$imageResourceGroup = "RG" + $timestamp
 
-# Distribution properties of the managed image upon completion
-$runOutputName = 'myDistResults'
+# Location (see possible locations in the main docs)
+$location = "centralindia"
 
-# Your Azure Subscription ID
+# Your subscription. This command gets your current subscription
+#Set Subscription
 $subscriptionID = (Get-AzContext).Subscription.Id
-Write-Output $subscriptionID
+Set-AzContext -SubscriptionId $subscriptionID
 
+# Image template name
+$imageTemplateName = "avd10ImageTemplate01"
+
+# Distribution properties object name (runOutput). Gives you the properties of the managed image on completion
+$runOutputName = "sigOutput"
+
+# Create resource group
 New-AzResourceGroup -Name $imageResourceGroup -Location $location
 
-$imageRoleDefName = "Azure Image Builder Image Def $timestamp"
-$identityName = "myIdentity$timestamp"
-
-New-AzUserAssignedIdentity -ResourceGroupName $imageResourceGroup -Name $identityName -Location $location
-
-$identityNameResourceId = (Get-AzUserAssignedIdentity -ResourceGroupName $imageResourceGroup -Name $identityName).Id
-$identityNamePrincipalId = (Get-AzUserAssignedIdentity -ResourceGroupName $imageResourceGroup -Name $identityName).PrincipalId
-
-
-$myRoleImageCreationUrl = 'https://raw.githubusercontent.com/spoddar13/azure_imagebuilder/main/RoleImageCreation.json'
-$myRoleImageCreationPath = "myRoleImageCreation.json"
-
-Invoke-WebRequest -Uri $myRoleImageCreationUrl -OutFile $myRoleImageCreationPath -UseBasicParsing
-
-$Content = Get-Content -Path $myRoleImageCreationPath -Raw
-$Content = $Content -replace '<subscriptionID>', $subscriptionID
-$Content = $Content -replace '<rgName>', $imageResourceGroup
-$Content = $Content -replace 'Azure Image Builder Service Image Creation Role', $imageRoleDefName
-$Content | Out-File -FilePath $myRoleImageCreationPath -Force
-
-# setup role def names, these need to be unique
+#setup role def names, these need to be unique
+$timeInt = $(get-date -UFormat "%s")
 $imageRoleDefName = "Azure Image Builder Image Def" + $timestamp
 $identityName = "aibIdentity" + $timestamp
 
@@ -58,8 +45,10 @@ New-AzUserAssignedIdentity -ResourceGroupName $imageResourceGroup -Name $identit
 $identityNameResourceId = $(Get-AzUserAssignedIdentity -ResourceGroupName $imageResourceGroup -Name $identityName).Id
 $identityNamePrincipalId = $(Get-AzUserAssignedIdentity -ResourceGroupName $imageResourceGroup -Name $identityName).PrincipalId
 
+
+
 $aibRoleImageCreationUrl = "https://raw.githubusercontent.com/spoddar13/azure_imagebuilder/main/RoleImageCreation.json"
-$aibRoleImageCreationPath = "MyRoleImageCreation.json"
+$aibRoleImageCreationPath = "aibRoleImageCreation.json"
 
 # Download the config
 Invoke-WebRequest -Uri $aibRoleImageCreationUrl -OutFile $aibRoleImageCreationPath -UseBasicParsing
@@ -69,7 +58,7 @@ Invoke-WebRequest -Uri $aibRoleImageCreationUrl -OutFile $aibRoleImageCreationPa
 ((Get-Content -path $aibRoleImageCreationPath -Raw) -replace 'Azure Image Builder Service Image Creation Role', $imageRoleDefName) | Set-Content -Path $aibRoleImageCreationPath
 
 # Create a role definition
-New-AzRoleDefinition -InputFile  ./MyRoleImageCreation.json
+New-AzRoleDefinition -InputFile  ./aibRoleImageCreation.json
 
 # Grant the role definition to the VM Image Builder service principal
 New-AzRoleAssignment -ObjectId $identityNamePrincipalId -RoleDefinitionName $imageRoleDefName -Scope "/subscriptions/$subscriptionID/resourceGroups/$imageResourceGroup"
