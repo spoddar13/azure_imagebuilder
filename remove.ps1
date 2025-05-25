c40b4028375dc7eccb5d93c3bf0cca3838563275
Sourav Poddar <sp.198713@gmail.com>
2025-05-25 17:39:44 +0530

    Customization Removed

--- remove.ps1
+++ remove.ps1
@@ -7,5 +7,5 @@ $resourceGroups
 'Resource groups Delete in Progress --'
 foreach ($group in $resourceGroups) {
     Write-Host "Selected ResourceGroup Name" $group 
-    az group delete --name $group #--yes   
+    az group delete --name $group --yes   
 }
